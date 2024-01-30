b217e716ee6123c0d52872c845fbd39cfd30046c
igorbdaluz <igorbdaluz@hotmail.com>
2024-01-30 17:56:00 -0300

    v1.0.1

--- GeminiCSharp/obj/Debug/netstandard2.0/GeminiCSharp.AssemblyInfo.cs
+++ GeminiCSharp/obj/Debug/netstandard2.0/GeminiCSharp.AssemblyInfo.cs
@@ -13,14 +13,14 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Igor Bock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n\t\t\tBiblioteca para uso da IA Google Bard através da API Gemini. Saiba mais no G" +
-    "itHub.\r\n\r\n\t\t\tLibrary for using Google Bard AI through the Gemini API. Learn more" +
-    " on GitHub.\r\n\t\t")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n\t\t\tLibrary for using Google Bard AI through the Gemini API. Learn more on GitHu" +
+    "b.\r\n\r\n\t\t\tBiblioteca para uso da IA Google Bard através da API Gemini. Saiba mais" +
+    " no GitHub.\r\n\t\t")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeminiCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeminiCSharp")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/igorbock/GeminiCSharp")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
