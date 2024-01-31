544a785b4a8bdeef2f3179ccbaeca88f0eafa471
igorbdaluz <igorbdaluz@hotmail.com>
2024-01-31 11:54:44 -0300

    AddConsoleClient

---
+++ ConsoleGeminiIA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using GeminiCSharp;
+
+var menu = "Faça sua pergunta para o Bard: \n\n'[X]' - Sair\n'[L]' - Limpar / Nova conversa\n\n";
+Console.WriteLine(menu);
+var nugetKey = ConfigurationManager.AppSettings["NUGET_KEY"];
+var geminiChat = new GeminiChat(nugetKey);
+
+var text = string.Empty;
+while (text != "[X]")
+{
+    using var httpClient = new HttpClient();
+
+    Console.Write("[Você]: ");
+    text = Console.ReadLine();
+    if (text == "[X]") break;
+    if (text == "[L]")
+    {
+        geminiChat.ResetToNewChat();
+        Console.Clear();
+        Console.WriteLine(menu);
+        continue;
+    }
+
+    var response = await geminiChat.SendMessageAsync(text, httpClient);
+    Console.WriteLine($"[Bard]: {response}");
+}
